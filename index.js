980facea6e6d960b31e86845326d5d314dc6c9cc
gladeSBHS <80649663+gladeSBHS@users.noreply.github.com>
2023-08-23 14:53:04 +1200

    stylesheet added ,margin gone, js file used
    removed margin, stylesheet added and linked, linked JavaScript file

---
+++ index.js
@@ -0,0 +1,6 @@
+const canvas =document.querySelector('canvas')
+const c = document.getElementById("canvas")
+const ctx = canvas.getContext("2d")
+
+canvas.width = innerWidth
+canvas.height = innerHeight
