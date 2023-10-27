7fa7fef5a772f9bf8e75155b7442f591529ac74c
gladeSBHS <80649663+gladeSBHS@users.noreply.github.com>
2023-10-27 19:11:39 +1300

    player stops moving when keys arent pressed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ canvas.height = innerHeight
 
 class Player {
     constructor() {
-       
+
 
         this.velocity = {
             x: 0,
@@ -15,7 +15,7 @@ class Player {
         }
         const image = new Image()
         image.src = './images/player.png'
-        image.onload = ()=>{
+        image.onload = () => {
             const scale = 0.25
             this.image = image
             this.width = image.width * scale
@@ -31,7 +31,7 @@ class Player {
         // c.fillStyle = 'red'
         // c.fillRect(this.position.x,this.position.y,
         //    this.width,this.height)
-        
+
         c.drawImage(
             this.image,
             this.position.x,
@@ -39,16 +39,27 @@ class Player {
             this.width,
             this.height)
     }
-    update(){
-        if(this.image) {
-        this.draw()
-        this.position.x += this.velocity.x
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
         }
     }
 }
 
 
 const player = new Player()
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 
 
 function animate() {
@@ -56,21 +67,45 @@ function animate() {
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update()
+    if (keys.a.pressed) {
+        player.velocity.x = -5
+    }else if (keys.d.pressed) {
+        player.velocity.x = 5
+    }else {
+        player.velocity.x = 0
+    }
 }
 animate()
 
- addEventListener('keydown', ({key}) => {
-     switch (key) {
+addEventListener('keydown', ({key}) => {
+    switch (key) {
         case 'a':
             console.log('left')
-            player.velocity.x = -5
+            keys.a.pressed = true
             break
         case 'd':
             console.log('right')
-            player.velocity.x = 5
+            keys.d.pressed = true
             break
         case ' ':
             console.log('space')
+            keys.space.pressed = true
             break
-     }
- })
+    }
+})
+addEventListener('keyup', ({key}) => {
+    switch (key) {
+        case 'a':
+            console.log('left')
+            keys.a.pressed = false
+            break
+        case 'd':
+            console.log('right')
+            keys.d.pressed = false
+            break
+        case ' ':
+            console.log('space')
+            keys.space.pressed = false
+            break
+    }
+})
