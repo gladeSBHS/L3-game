dca748d7e78579b6e217ad9aa85e8425ce617966
gladeSBHS <80649663+gladeSBHS@users.noreply.github.com>
2023-10-29 20:45:57 +1300

    made the column amount random

--- index.js
+++ index.js
@@ -139,8 +139,9 @@ class Grid {
             y: 0
         }
         this.invaders = []
+        const columns = Math.floor(Math.random() * 10 + 5)
         const rows = Math.floor(Math.random() * 5 + 2)
-        for (let x = 0; x < 10; x++) {
+        for (let x = 0; x < columns; x++) {
             for (let y = 0; y < rows; y++) {
                 this.invaders.push(new Invader({
                     position: {
