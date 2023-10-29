82609b9af68e9fab8c1ad5e93b0f28f9c007d974
gladeSBHS <80649663+gladeSBHS@users.noreply.github.com>
2023-10-29 21:03:05 +1300

    invaders move down 30px when they bounce of canvas

--- index.js
+++ index.js
@@ -162,8 +162,11 @@ class Grid {
         this.position.x += this.velocity.x
         this.position.y += this.velocity.y
 
+        this.velocity.y = 0
+
         if (this.position.x + this.width >= canvas.width || this.position.x <= 0) {
             this.velocity.x = -this.velocity.x
+            this.velocity.y = 30
         }
     }
 }
